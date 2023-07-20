ba0f0127d59d9ef77221481588f9db8bc18687de
Татьяна Кочубейник <kisa7392@mail.ru>
2023-07-20 21:07:44 +0300

    First version

---
+++ Lesson 1/Task 1/Task 1/sql_query_builder.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+struct QueryBuilder {
+	std::vector <std::string> columns;
+	std::vector <std::string> tables;
+	std::map <std::string, std::string> selection;
+};
+
+class SqlSelectQueryBuilder {
+public:
+	SqlSelectQueryBuilder() = default;
+	SqlSelectQueryBuilder& AddColumn(const std::string& column_name) noexcept {
+		querybuilder.columns.push_back(column_name);
+		return *this;
+	}
+	SqlSelectQueryBuilder& AddFrom(const std::string& table_name) noexcept {
+		querybuilder.tables.push_back(table_name);
+		return *this;
+	}
+	SqlSelectQueryBuilder& AddWhere(const std::string& column, const std::string& value) noexcept {
+		querybuilder.selection[column] = value;
+		return *this;
+	}
+
+	std::string BuildQuery() {
+		std::string res = "SELECT ";
+		if (querybuilder.columns.empty()) {
+			res += "*";
+		}
+		for (int i = 0; i < querybuilder.columns.size(); i++) {
+			res += querybuilder.columns[i];
+			if (i < querybuilder.columns.size() - 1) {
+				res += ", ";
+			}
+		}
+		if (!querybuilder.tables.empty()) {
+			res += " FROM ";
+			for (int i = 0; i < querybuilder.tables.size(); i++) {
+				res += querybuilder.tables[i];
+				if (i < querybuilder.tables.size() - 1) {
+					res += ", ";
+				}
+			}
+		}
+		if (!querybuilder.selection.empty()) {
+			res += " WHERE ";
+			for (const auto& elem : querybuilder.selection) {
+				res += elem.first + "=" + elem.second + " AND ";
+			}
+			res = res.substr(0, res.size() - 5);
+		}
+		return res + ";";
+	}
+private:
+	QueryBuilder querybuilder;
+};
---
+++ Lesson 1/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "sql_query_builder.h"
+
+int main() {
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn("name").AddColumn("phone");
+    std::vector<std::string> c = { "email", "address" };
+    query_builder.AddColumns(c);
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+    std::map<std::string, std::string> t = { { "phone","111111" }, { "email", "http.st" } };
+    query_builder.AddWhere(t);
+    std::cout << query_builder.BuildQuery() << "\n";
+    return 0;
+}
