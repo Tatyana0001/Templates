90bd587526155953bb9d21ffdfc7f18c6abff076
Татьяна Кочубейник <kisa7392@mail.ru>
2023-09-02 14:08:42 +0300

    First version

---
+++ Lesson 4/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,92 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    // Нарушен принцип разделения интерфейса. 
+    // Необходимо создавать несколько узкоспециализированных
+    // интерфейсов. Создать в этом классе один метод, например 
+    // Print  и уже наследоваться от этого класса.
+
+    virtual std::string printAsHTML() const = 0;
+    virtual std::string printAsText() const = 0;
+    virtual std::string printAsJSON() const = 0;
+};
+
+class Data : public Printable
+{
+public:
+    enum class Format
+    {
+        kText,
+        kHTML,
+        kJSON
+    };
+
+    Data(std::string data, Format format)
+        : data_(std::move(data)), format_(format) {}
+
+
+    // Нарушен принцип подстановки Лисков, так как
+    // printAsHTML, printAsText и printAsJSON 
+    // выбрасывают исключение.
+    std::string printAsHTML() const override
+    {
+        if (format_ != Format::kHTML) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "<html>" + data_ + "<html/>";
+    }
+    std::string printAsText() const override
+    {
+        if (format_ != Format::kText) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return data_;
+    }
+    std::string printAsJSON() const override
+    {
+        if (format_ != Format::kJSON) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+
+private:
+    std::string data_;
+    Format format_;
+};
+
+void saveTo(std::ofstream& file, const Printable& printable, Data::Format format)
+{
+    // Нарушен принцип открытости/закрытости, так как
+    // при добавлении нового формата, ведет к изменению
+    // отлаженного кода saveTo.
+    
+    switch (format)
+    {
+    case Data::Format::kText:
+        file << printable.printAsText();
+        break;
+    case Data::Format::kJSON:
+        file << printable.printAsJSON();
+        break;
+    case Data::Format::kHTML:
+        file << printable.printAsHTML();
+        break;
+    }
+}
+
+void saveToAsHTML(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kHTML);
+}
+
+void saveToAsJSON(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kJSON);
+}
+
+void saveToAsText(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kText);
+}
---
+++ Lesson 4/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,65 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+    virtual std::string Print() const = 0;
+};
+
+class PrintAsHTML : public Printable
+{
+public:
+    PrintAsHTML(std::string data) : data_(std::move(data)) {}
+    ~PrintAsHTML() override = default;
+    std::string Print() const override {
+        return "<html>" + data_+ "<html/>";
+    }
+private:
+    std::string data_;
+};
+
+class PrintAsText : public Printable
+{
+public:
+    PrintAsText(std::string data) : data_(std::move(data)) {}
+    ~PrintAsText() override = default;
+    std::string Print() const override {
+        return data_;
+    }
+private:
+    std::string data_;
+};
+
+class PrintAsJSON : public Printable
+{
+public:
+    PrintAsJSON(std::string data) : data_(std::move(data)) {}
+    ~PrintAsJSON() override = default;
+    std::string Print() const override {
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+private:
+    std::string data_;
+};
+
+void SaveTo(std::ofstream& file, const Printable& printable) {
+    file << printable.Print();
+}
+
+int main() {
+    PrintAsHTML printHTML("HTML");
+    PrintAsText printText("Text");
+    PrintAsJSON printJSON("JSON");
+    std::ofstream out("out.txt");
+    if (out.is_open()) {
+        SaveTo(out, printHTML);
+        out << "\n";
+        SaveTo(out, printText);
+        out << "\n";
+        SaveTo(out, printJSON);
+        out << "\n";
+        out.close();
+    }
+    return 0;
+}
